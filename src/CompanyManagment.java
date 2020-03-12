75e7be8cc8b4b63e23ae78b5935f23b2b574d091
joesupagorn <supagorn.tong2541@gmail.com>
2020-03-12 09:49:01 +0700

    Employee

--- src/CompanyManagment.java
+++ src/CompanyManagment.java
@@ -6,9 +6,50 @@ public class CompanyManagment {
         //creat instance
         EmployeeDAOImpl dao=EmployeeDAOImpl.getInstance();
         //display all employee
-        displayAllEmployee(dao);
+       // displayAllEmployee(dao);
         //addNewEmployee
-        addNewEmployee(dao);
+        //addNewEmployee(dao);
+        //find employee by ID
+        //findEmployeeID(dao);
+        //update employee by ID
+       // updateEmployeeByID(dao);
+        //dlete employee by ID
+        deletEmployeeByID(dao);
+    }
+
+    private static void deletEmployeeByID(EmployeeDAOImpl dao) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter an employee ID that you want to delete:");
+        int id =Integer.parseInt(sc.nextLine().trim());
+        dao.deletEmp(id);
+    }
+
+    private static void updateEmployeeByID(EmployeeDAOImpl dao) {
+        //Search employee ID
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter and employee ID:");
+        int id = Integer.parseInt(sc.nextLine());
+        Employee emp = dao.findEmp(id);
+        if (emp != null) {
+            System.out.println(emp.toString());
+            //get new data
+            System.out.print("Enter new salary for employee ID"
+                    + emp.getEmpID() + ":");
+            double ns = Double.parseDouble(sc.nextLine().trim());
+            //edit data
+            emp.setSalary(ns);
+            //Update data into database
+            dao.updateEmp(emp);
+        }
+    }
+    private static void findEmployeeID(EmployeeDAOImpl dao) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter and employee ID:");
+        int id = Integer.parseInt(sc.nextLine());
+        Employee emp = dao.findEmp(id);
+        if (emp != null) {
+            System.out.println(emp.toString());
+        }
     }
 
     private static void addNewEmployee(EmployeeDAOImpl dao) {
